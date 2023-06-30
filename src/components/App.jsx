631e86afec3d2ef900290f0727687babb155af6b
Eleonora907 <elenatroyan2010@ukr.net>
2023-06-30 13:23:09 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,19 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      this.setState({ contacts: JSON.parse(storedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   isContactDuplicate = newContact => {
     return this.state.contacts.some(
       contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
