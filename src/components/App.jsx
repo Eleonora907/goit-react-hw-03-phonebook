ddfed0269ef445abacf4398abe728b315cdc7205
Eleonora907 <elenatroyan2010@ukr.net>
2023-07-01 20:46:13 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,9 +6,20 @@ import Notiflix from 'notiflix';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts:  [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
     filter: '',
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   componentDidMount() {
     const storedContacts = localStorage.getItem('contacts');
@@ -17,11 +28,7 @@ export class App extends Component {
     }
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+
 
   isContactDuplicate = newContact => {
     return this.state.contacts.some(
